78a1f88a251296e58d2774ea2997910e2298ed92
Luis <luisovalle1735@yahoo.com>
2022-07-22 17:26:48 -0600

    commit tree 06c489d76283bd2b851b78e9f2770e9b57cf2384

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "Luis",
+    streetAddress: "182 Wexford"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return['name', ... 'Luis']
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    return[employee, key, value, ... key.pop()]
+}
